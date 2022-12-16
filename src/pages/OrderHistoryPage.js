beef3a5a241efeda00c3b1a4f4326c4ba95a7c34
Isaiah Bens <benavidesisaiah@outlook.com>
2022-12-16 10:40:04 -0500

    show expDate

--- src/pages/OrderHistoryPage.js
+++ src/pages/OrderHistoryPage.js
@@ -1,14 +1,22 @@
 import { checkToken } from "../utilities/users-service";
+import { useState } from "react"
 
 export default function OrderHistoryPage(props) {
+    const [expDate, setExpDate] = useState("")
   const handleCheckToken = async () => {
     const expDate = await checkToken();
+    setExpDate(expDate)
   };
 
   return (
     <>
       <h1>Order History Page</h1>
       <button onClick={handleCheckToken}>Check When My Login Expires</button>
+      {
+        expDate ? <p>{expDate.toLocaleString()}</p> 
+        :
+        ""
+      }
     </>
   );
 }
