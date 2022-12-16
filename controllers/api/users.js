b1d2b3d1ca78e8e62ea4ae1f8f6b8d13ae14d2c8
Isaiah Bens <benavidesisaiah@outlook.com>
2022-12-16 09:15:50 -0500

    LogInForm

--- controllers/api/users.js
+++ controllers/api/users.js
@@ -1,8 +1,10 @@
 const User = require('../../models/user')
 const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt')
 
 module.exports = {
-    create
+    create,
+    login
 }
 async function create(req, res) {
     try {
@@ -13,6 +15,20 @@ async function create(req, res) {
         // Yes, we can use res.json to send back just a string
         // The client code needs to take this into consideration
         res.json(token);
+    } catch (error) {
+        // Client will check for non-2xx status code
+        // 400 = bad request
+        res.status(400).json(error)
+    }
+}
+
+async function login(req, res) {
+    try {
+        const user = await User.findOne({email: req.body.email})
+        if (!user) throw new Error()
+        const match = await bcrypt.compare(req.body.password, user.password)
+        if (!match) throw new Error()
+        res.json(createJWT(user))
     } catch (error) {
         // Client will check for non-2xx status code
         // 400 = bad request
--- src/pages/AuthPage.js
+++ src/pages/AuthPage.js
@@ -1,10 +1,12 @@
 import SignUpForm from "../components/SignUpForm"
+import LoginForm from "../components/LogInForm"
 
 export default function AuthPage(setUser) {
     return (
         <main>
             <h1>Authorization Page</h1>
             <SignUpForm setUser={setUser}/>
+            <LoginForm setUser={setUser}/>
         </main>
     )
 }
